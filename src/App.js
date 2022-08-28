34e9ce66526712cc5e0335074141e567625491e0
MahmoudRedaSayed <anamahmoud10203040@gmail.com>
2022-08-28 09:12:44 +0200

    get the user set the user in the database

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React from "react";
 import "./App.css";
 import useWindowSize from "./hooks/useWindowSize";
 import Login from "./components/Login";
+import useAuthUser from "./hooks/useAuthUser";
 
 export default function App() {
   const page = useWindowSize();
-  return <Login></Login>
+  const user=useAuthUser();
+  if(!user)
+  {
+    return <Login></Login>
+  }
   return (
     <div className="app" style={{ ...page }}>
       App
--- src/hooks/useAuthUser.js
+++ src/hooks/useAuthUser.js
@@ -1,1 +1,26 @@
-export default function useAuthUser() {}
+import { useEffect } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { db,auth,createTimeStamp } from "../firebase";
+export default function useAuthUser() {
+    const [user] = useAuthState(auth);
+    useEffect(()=>{
+        if(user)
+        {
+            console.log(user);
+            const ref=db.collection("users").doc(user.uid);
+            ref.get().then((doc)=>{console.log(doc)
+                if(!doc.exists)
+                {
+                    ref.set({
+                        name:user.displayName,
+                        photo:user.photoURL,
+                        timestamp:createTimeStamp()
+                    })
+                }
+            
+            })
+
+        }
+    }) 
+    return user 
+}
